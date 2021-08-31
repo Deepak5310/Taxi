76f52c7dfa7a40c8391410a208eded771c00a50b
Deepak5310 <deepakjangir5310@gmail.com>
2021-08-31 16:23:13 +0530

    Add splash screen

---
+++ app/src/main/java/com/deedev/taxi/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.deedev.taxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splash = new Thread() {
+            public void run() {
+                try {
+                    sleep(3 * 500);
+                    Intent i = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception ignored) {
+                }
+            }
+        };
+        splash.start();
+    }
+}
